b360ab1fbe1641b2165395c036a70ffd8df5ac62
Zuzanna Mejer <101196834+zuzmej@users.noreply.github.com>
2025-01-19 18:34:58 +0100

    Add Package class with PackageStates enumeration (#12)

---
+++ Package.py
@@ -0,0 +1,25 @@
+import ObjectBase
+from enum import Enum
+from typing import Optional
+
+class PackageStates(Enum):
+    IN_SORTING_PLANT = "IN_SORTING_PLANT"
+    IN_TRANSPORT = "IN_TRANSPORT"
+    IN_PACKAGE_STATION = "IN_PACKAGE_STATION"
+    COLLECTED = "COLLECTED"
+    EXPIRED = "EXPIRED"
+
+class Package(ObjectBase):
+    def __init__(self, package_id: int, package_station_id: int):
+        super().init(package_id)
+        self._package_station_id = package_station_id
+        self._state = PackageStates.IN_SORTING_PLANT
+        self._loading_date: Optional[float] = None
+        self._expiry_date: Optional[float] = None
+
+    def get_state(self) -> PackageStates:
+        return self._state
+
+    def set_state(self, new_state: PackageStates) -> bool:
+        self._state = new_state
+        return True
